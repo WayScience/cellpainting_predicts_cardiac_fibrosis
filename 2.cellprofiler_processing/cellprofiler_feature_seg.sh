2fcb91d5d4d1a014e8e5576631ec4014474633f8
jenna-tomkinson <jenna.tomkinson@ucdenver.edu>
2023-05-16 09:57:45 -0600

    remove old files that are not needed

--- 2.cellprofiler_processing/cellprofiler_feature_seg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# convert jupyter notebook to a .py file once all variable changes are made
-jupyter nbconvert --to python example_cp.ipynb
-
-# run notebook through Python
-python example_cp.py
