4f3ee70e13889e59386b8167597855fc434b43cb
jenna-tomkinson <jenna.tomkinson@ucdenver.edu>
2023-09-14 13:02:01 -0600

    add refactored notebooks and processing module

---
+++ 3.process_cfret_features/process_data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# initialize the correct shell for your machine to allow conda to work (see README for note on shell names)
+conda init bash
+# activate the main conda environment
+conda activate cfret_data_env
+
+# convert all notebooks to python files into the scripts folder
+jupyter nbconvert --to python --output-dir=scripts/ *.ipynb
+
+# run python script to preprocess data before downstream analysis (ran in sequential order)
+python scripts/0.convert_cytotable.py
+python scripts/1.single_cell_processing.py
